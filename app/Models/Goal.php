5fe6e719036741d64b9b5dfec5a57cd62c1da652
patrickMaureal <janpatrickmaureal@gmail.com>
2025-01-08 11:35:14 +0800

    add Model and Controller for Goal Feature

---
+++ app/Models/Goal.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Goal extends Model
+{
+	use HasUuids, SoftDeletes;
+  
+	protected $fillable = [
+    'user_id', 'name', 'target_amount', 'current_amount','start_date', 'end_date','status'
+  ];
+}
